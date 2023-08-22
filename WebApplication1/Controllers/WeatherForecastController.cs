903965941385d7ee8aca9794213905175f941c7c
Markeesi <62094844+Markeesi@users.noreply.github.com>
2023-08-22 15:17:11 +0300

    Add project files.

---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Net.Http;
+
+[ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly HttpClient _httpClient;
+
+    public WeatherForecastController(IHttpContextAccessor httpContextAccessor, IHttpClientFactory httpClientFactory)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _httpClient = httpClientFactory.CreateClient();
+        }
+        
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task<IEnumerable<string>> Get()
+        {
+
+
+
+        Console.WriteLine("Got in WebApi1");
+        if (_httpContextAccessor.HttpContext !=null)
+        {
+            string requestingUrl = _httpContextAccessor.HttpContext.Request.GetDisplayUrl();
+            string logEntry = $"Request from: {requestingUrl} at {DateTime.Now}";
+            await System.IO.File.AppendAllLinesAsync("log1.txt", new[] { logEntry });
+        }
+           else
+
+        {
+            Console.WriteLine("HttpContext is null!");
+        }
+
+       // System.IO.File.AppendAllLines("log1.txt", new[] { "Get called at " + DateTime.Now + " " +Environment.GetEnvironmentVariables(EnvironmentVariableTarget.User) });
+
+        System.Threading.Thread.Sleep(400);
+        await CallOtherApi();
+        return new[]
+            {
+             "moi!"
+            };
+            
+        }
+
+    [HttpGet("CallOtherApi")]
+    public async Task<IActionResult> CallOtherApi()
+    {
+        string apiUrl = "https://localhost:7257/WeatherForecast"; // Replace with your WebApplication2 URL
+
+        // Log the request information
+        string logEntry = $"Calling WebApplication2 API at {apiUrl} from {Request.GetDisplayUrl()} at {DateTime.Now}";
+        await System.IO.File.AppendAllLinesAsync("log.txt", new[] { logEntry });
+
+        HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            return Ok(apiResponse);
+        }
+        else
+        {
+            return StatusCode((int)response.StatusCode);
+        }
+    }
+
+
+
+};
